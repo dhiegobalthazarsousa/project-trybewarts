0bdddfb0e7dc57cee9285651b0adb2afa2969b46
Dhiego Balthazar <dhiegobalthazar@gmail.com>
2023-01-17 20:21:32 -0300

    Feat: resolve parte do requisito 21

--- script.js
+++ script.js
@@ -6,11 +6,27 @@ const checkbox = document.querySelector('#agreement');
 const textArea = document.querySelector('.comentario');
 const characterCounter = document.querySelector('#counter');
 
+const nome = document.querySelector('#nome');
+const email = document.querySelector('#email');
+const casa = document.querySelector('#casa');
+const familia = document.querySelector('#familia');
+const materias = document.querySelector('#materias');
+const avaliacao = document.querySelector('#avaliacao');
+const observacao = document.querySelector('#observacao');
+const inputName = document.querySelector('#input-name');
+const inputLastName = document.querySelector('#input-lastname');
+const inputEmail = document.querySelector('#input-email');
+const inputCasa = document.querySelector('#house');
+const inputFamilia = document.querySelector('input[name="family"]:checked');
+const inputMateria = document.querySelectorAll('input[name="materia"]:checked');
+const inputAvaliacao = document.querySelector('input[name="rate"]:checked');
+const inputObservacao = document.querySelector('#textarea');
+
 const validateForm = (event) => {
   event.preventDefault();
-  const email = emailLogin.value;
+  const emailValidate = emailLogin.value;
   const senha = senhaLogin.value;
-  if (email === 'tryber@teste.com' && senha === '123456') {
+  if (emailValidate === 'tryber@teste.com' && senha === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
@@ -33,14 +49,30 @@ const isEmpty = () => {
   }
 };
 
-const counterCharacterInTextArea = (event) => {
+const counterCharacterInTextArea = () => {
   characterCounter.innerText = 500 - textArea.value.length;
-}
+};
+
+const submitValues = (event) => {
+  event.preventDefault();
+  nome.innerText = `Nome: ${inputName.value} ${inputLastName.value}`;
+  email.innerText = `Email: ${inputEmail.value}`;
+  casa.innerText = `Casa: ${inputCasa.value}`;
+  familia.innerText = `Família: ${inputFamilia}`;
+  let stringMaterias = '';
+  for (let index = 0; index < inputMateria.length; index += 1) {
+    stringMaterias += inputMateria[index].value;
+  }
+  materias.innerText = `Matérias: ${stringMaterias}`;
+  avaliacao.innerText = `Avaliação: ${inputAvaliacao}`;
+  observacao.innerText = `Observações: -${inputObservacao.value}-`;
+};
 
 const callListeners = () => {
   botaoLogin.addEventListener('click', validateForm);
   checkbox.addEventListener('click', isEmpty);
   textArea.addEventListener('input', counterCharacterInTextArea);
+  botaoEnviar.addEventListener('click', submitValues);
 };
 
 window.onload = () => {
